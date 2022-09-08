6c4344975605fa61051b167de0b8e8b90554db81
ngoc-doan-athena <110010178+ngoc-doan-athena@users.noreply.github.com>
2022-09-08 11:32:13 +0700

    Add files via upload

--- src/pages/MyPortfolio.js
+++ src/pages/MyPortfolio.js
@@ -7,14 +7,14 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 
 import { Container as ContainerBase } from "../components/Layouts";
 import Sidebar from "../components/SideBar/index.js";
-import Header from "../components/Header/index.js";
+import HeaderRoot from "../components/Header/HeaderRoot.js";
 import { BoxBase as Box} from "../components/Box/index.js";
 
 const Container = tw(
 	ContainerBase
-)`min-h-screen font-medium flex justify-center m-0 font-inter`;
-const Content = tw.div`bg-gray-100 dark:bg-black text-black dark:text-white flex flex-col lg:flex-row justify-center flex-1 relative`;
-const MainContent = tw.div`flex-1 px-12`;
+)`h-screen lg:min-h-screen font-medium flex justify-center m-0 font-inter`;
+const Content = tw.div`bg-gray-100 dark:bg-black text-black dark:text-white w-full lg:w-auto flex flex-col lg:flex-row justify-center flex-1 relative`;
+const MainContent = tw.div`flex-1 p-4 pt-0 lg:px-12 lg:pb-8 order-first lg:order-last`;
 
 export default ({
 	headingText = "My Portfolio",
@@ -24,7 +24,7 @@ export default ({
 			<Content>
 				<Sidebar />
 				<MainContent>
-					<Header headingText={headingText} />
+					<HeaderRoot headingText={headingText} />
 					<Box />
 				</MainContent>
 			</Content>
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+
 import tw from "twin.macro";
 import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
 
-import { Container as ContainerBase } from "../../components/Layouts.js";
+import { Container as ContainerBase } from "../../components/Layouts";
 import Sidebar from "../../components/SideBar/index.js";
+import HeaderRoot from "../../components/Header/HeaderRoot.js";
+import { BoxBase as Box} from "../../components/Box/index.js";
 
 const Container = tw(
 	ContainerBase
-)`min-h-screen font-medium flex m-0 bg-gray-100`;
+)`h-screen lg:min-h-screen font-medium flex justify-center m-0 font-inter`;
+const Content = tw.div`bg-gray-100 dark:bg-black text-black dark:text-white w-full lg:w-auto flex flex-col lg:flex-row justify-center flex-1 relative`;
+const MainContent = tw.div`flex-1 p-4 pt-0 lg:px-12 lg:pb-8 order-first lg:order-last`;
 
-export default () => {
+export default ({
+	headingText = "Contact Us",
+}) => {
 	return (
 		<Container>
-			<Sidebar />
-			<Outlet />
+			<Content>
+				<Sidebar />
+				<MainContent>
+					<HeaderRoot headingText={headingText} />
+					<Box />
+				</MainContent>
+			</Content>
 		</Container>
 	);
 };
